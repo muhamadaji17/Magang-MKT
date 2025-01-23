af71d71c39f5dedb328741ebd09f0d10a1b65fca
Azed Muhammad Noval <azedmuhammadn@gmail.com>
2025-01-23 23:38:19 +0700

    Deleted file

--- src/pattern/handleButton.js
+++
@@ -1,7 +0,0 @@
-export const handleSubmitData = (data, postData) => {
-  postData(data);
-};
-
-export const handleShowPassword = (setShowPassword, showPassword) => {
-  setShowPassword(!showPassword);
-};
